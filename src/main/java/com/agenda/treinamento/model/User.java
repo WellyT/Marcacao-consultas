78851b6ff41ec69298cdb5e45d7e3f3cc9012760
Wellyon Thomas <47388425+WellyT@users.noreply.github.com>
2020-02-19 05:18:51 -0300

    Starting application, first view and first class

---
+++ src/main/java/com/agenda/treinamento/model/User.java
@@ -0,0 +1,37 @@
+package com.agenda.treinamento.model;
+
+public class User {
+
+	private String nome;
+	private String cpf;
+	private int idade;
+	private String email;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
